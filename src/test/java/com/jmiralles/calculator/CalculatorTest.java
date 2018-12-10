c3ae17bf2db570a1935d52a392c6ff7d9e90585b
jmiralles <jmirallespaez@gmail.com>
2018-12-10 12:47:45 +0100

    added datasource for multiple test in add_two_numbers

--- src/test/java/com/jmiralles/calculator/CalculatorTest.java
+++ src/test/java/com/jmiralles/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package com.jmiralles.calculator;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 import static org.testng.Assert.*;
 
@@ -12,6 +13,11 @@ public class CalculatorTest {
     @BeforeTest
     public void setUp() {
         calculator = new Calculator();
+    }
+
+    @DataProvider
+    public Object[][] dataMethod() {
+        return new Object[][] { { "1,2" , 3}, { "4, 8" , 12 }, { "9 ,10", 19} };
     }
 
     @Test
@@ -28,10 +34,10 @@ public class CalculatorTest {
         Assert.assertEquals(actual, expected);
     }
 
-    @Test
-    public void add_two_numbers() {
-        int expected = 10;
-        int actual = calculator.Add("2, 8");
+    @Test(dataProvider = "dataMethod")
+    public void add_two_numbers(String numbers, int result) {
+        int expected = result;
+        int actual = calculator.Add(numbers);
         Assert.assertEquals(actual, expected);
     }
 
