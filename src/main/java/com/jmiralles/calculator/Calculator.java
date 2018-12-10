ee5d34458c5d915caea5e377cd67f0e32279e886
jmiralles <jmirallespaez@gmail.com>
2018-12-10 12:50:37 +0100

    refactoring

--- src/main/java/com/jmiralles/calculator/Calculator.java
+++ src/main/java/com/jmiralles/calculator/Calculator.java
@@ -6,10 +6,14 @@ public class Calculator {
         if (numbers.length() == 0) {
             return 0;
         } else if (numbers.length() == 1) {
-            return Integer.parseInt(numbers);
+            return stringToNumber(numbers);
         } else {
             String[] parts = numbers.split(",");
-            return Integer.parseInt(parts[0].trim()) + Integer.parseInt(parts[1].trim());
+            return stringToNumber(parts[0].trim()) + stringToNumber(parts[1].trim());
         }
+    }
+
+    private int stringToNumber(String number) {
+        return Integer.parseInt(number);
     }
 }
